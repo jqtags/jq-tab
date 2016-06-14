2c24b5dfdb008c8fd2bdbf5712316e125e4e6ab1
Sahil <sahil@unicommerce.com>
2016-06-14 17:08:18 +0530

    scss compilation added

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# To compile indivisidual file
+# compass compile source-file --no-line-comments
+#Example: compass compile jqtags.tab.scss --no-line-comments
+
+# To watch scss file
+# compass watch
+
+# To compass scss file
+# compass compile
+
